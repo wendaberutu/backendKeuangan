68e78d80e7220ece2890bcb14381276cba641224
Damar <dtlagl77@gmail.com>
2025-06-22 19:55:56 +0700

    daftarkan middleware disini (cara baru)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,14 +6,19 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
+        health: '/up'
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
+    ->withMiddleware(function ($middleware) {
+        // Menambahkan alias untuk middleware
+        $middleware->alias([
+            'isAdmin' => \App\Http\Middleware\AdminMiddleware::class,
+            'isOwner' => \App\Http\Middleware\OwnerMiddleware::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        // Tangani pengecualian jika diperlukan
+    })
+    ->create();
