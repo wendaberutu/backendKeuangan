f22689c1246960e664f4a16c4e34e69ad6ff43f7
Damar <dtlagl77@gmail.com>
2025-06-27 15:30:48 +0700

    no bukti unique

--- database/migrations/2025_06_14_144146_create_transactions_table.php
+++ database/migrations/2025_06_14_144146_create_transactions_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->foreignId('account_id')->constrained()->cascadeOnDelete();
             $table->enum('tipe_transaksi', ['masuk', 'keluar']);
             $table->text('deskripsi')->nullable();
-            $table->text('no_bukti')->nullable();
-            $table->date('waktu');
+            $table->string('no_bukti', 50)->unique();
+            $table->date('waktu')->format('d-m-Y');
             $table->unsignedBigInteger('nominal');
             $table->timestamps();
         });
